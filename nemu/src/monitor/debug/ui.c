156a390a17751f9df3420554257f7d636d8321e0
tracer-ics2015 <tracer@njuics.org>
2015-09-26 15:56:06 +0800

    >  compile NEMU 141220000 yixiaodong Linux yixiaodong-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  15:56:05 up  1:09,  2 users,  load average: 0.33, 0.27, 0.24 8a22f2236e47c0f84c8f89f77184df3272fc6a2a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -152,7 +152,7 @@ static int cmd_x(char *args){
 			printf("Expression is wrong\n");
 		else
 			for(i=0;i<size;i++)
-			printf("%08x\t%08x\n",addr,swaddr_read(addr+i*4,4));
+			printf("%08x\t%08x\n",addr+i*4,swaddr_read(addr+i*4,4));
 	}
 	return 0;
 }
