1f58e9c9737bda984895cd4bf783e21cb0318ff4
tracer-ics2015 <tracer@njuics.org>
2015-10-25 22:39:57 +0800

    >  compile NEMU 141220000 yixiaodong Linux yixiaodong-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  22:39:56 up  1:17,  2 users,  load average: 0.59, 0.33, 0.28 5f8244de417f58cbd72101758db9194c3a32a0a

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,6 +28,30 @@ typedef struct {
 		};	
    	 };
 	swaddr_t eip;
+	
+	union {
+		struct{
+				uint32_t blank:14;
+				uint32_t VM:1;
+				uint32_t RF:1;
+				uint32_t bit_a:1;
+				uint32_t NT:1;
+				uint32_t IOPL:2;
+				uint32_t OF:1;
+				uint32_t DF:1;
+				uint32_t IF:1;
+				uint32_t TF:1;
+				uint32_t SF:1;
+				uint32_t ZF:1;
+				uint32_t b:1;
+				uint32_t AF:1;
+				uint32_t bit_c:1;
+				uint32_t PF:1;
+				uint32_t bit_d:1;
+				uint32_t CF:1;
+			    };
+			uint32_t eflags:32;
+		};
 
 } CPU_state;
 
