b67ceee746ffa560fd39e87731f9061d29d983f4
tracer-ics2015 <tracer@njuics.org>
2015-10-27 22:28:13 +0800

    >  compile NEMU 141220000 yixiaodong Linux yixiaodong-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  22:28:12 up  1:51,  2 users,  load average: 0.10, 0.04, 0.09 f5dd68ba69ef4bcf8aca4e0a3fb7f2744d9c7685

--- nemu/src/cpu/exec/arith/jbe-template.h
+++ nemu/src/cpu/exec/arith/jbe-template.h
@@ -1,27 +1,39 @@
 #include "cpu/exec/template-start.h"
-
+#include "cpu/reg.h"
 #define instr jbe
 
-static void do_execute() {
-	//DATA_TYPE result = ~op_src->val;
-	//OPERAND_W(op_src, result);
-	//cpu.esp -= 4;
-	//MEM_W(cpu.esp,cpu.eip);
-	//cpu.eip +=(int32_t)op_src->val;
-	//#if DATA_BYTE==2
-	  //   cpu.eip &= 0x0000ffff;
-	//#endif
-	if(cpu.ZF==1||cpu.CF==1)
-		{
-		  cpu.eip +=(int32_t)op_src->val;
-		  #if DATA_BYTE == 2
-		     cpu.eip &= 0x0000ffff;
-		  #endif
-		  
-		}
-	print_asm_template1();
+static void do_execute(){
+    if(cpu.CF==1||cpu.ZF==1){
+    #if DATA_BYTE==1
+        uint8_t val= (uint8_t)(cpu.eip&0xFF);
+        val+=(uint8_t)op_src->val;
+        cpu.eip=(cpu.eip&0xFFFFFF00)+val;
+    #elif   DATA_BYTE==2
+        uint16_t val= (uint16_t)(cpu.eip&0xFFFF);
+        val+=(uint16_t)op_src->val;
+        cpu.eip=(cpu.eip&0xFFFF0000)+val;
+    #else
+
+        cpu.eip=cpu.eip+op_src->val;
+    #endif // DATA_BYTE
+    }
+
+
+
+  /*  #if DATA_BYTE==2
+    cpu.eip=(cpu.eip+op_src->val)&0xFFFF;
+    #else
+    cpu.eip+=cpu.eip+(int32_t)op_src->val;
+    #endif // DATA_BYTE
+
+    print_asm_template1();
+
+*/
+
 }
 
-make_instr_helper(i)
 
+
+make_instr_helper(i)
 #include "cpu/exec/template-end.h"
+
--- nemu/src/cpu/exec/arith/je-template.h
+++ nemu/src/cpu/exec/arith/je-template.h
@@ -13,10 +13,18 @@ static void do_execute() {
 	//#endif
 	if(cpu.ZF==1)
 		{
-		  cpu.eip +=(int32_t)op_src->val;
-		  #if DATA_BYTE == 2
-		     cpu.eip &= 0x0000ffff;
-		  #endif
+		   #if DATA_BYTE==1
+        uint8_t val= (uint8_t)(cpu.eip&0xFF);
+        val+=(uint8_t)op_src->val;
+        cpu.eip=(cpu.eip&0xFFFFFF00)+val;
+    #elif   DATA_BYTE==2
+        uint16_t val= (uint16_t)(cpu.eip&0xFFFF);
+        val+=(uint16_t)op_src->val;
+        cpu.eip=(cpu.eip&0xFFFF0000)+val;
+    #else
+
+        cpu.eip=cpu.eip+op_src->val;
+    #endif // DATA_BYTE
 		  
 		}
 	print_asm_template1();
