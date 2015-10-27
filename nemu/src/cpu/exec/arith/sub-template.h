78b964a657208b393d1738c8cd3791e9b38a9799
tracer-ics2015 <tracer@njuics.org>
2015-10-27 20:55:17 +0800

    >  compile NEMU 141220000 yixiaodong Linux yixiaodong-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  20:55:17 up 18 min,  2 users,  load average: 0.46, 0.56, 0.45 773a5c3b5a163fb855dc93c8e2aaf394c8b721

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -8,6 +8,48 @@ static void do_execute () {
 
 	/* TODO: Update EFLAGS. */
 	//panic("please implement me");
+	int bit[8];
+	bit[0]=result&0x1;
+	bit[1]=(result>>1)&0x1;
+	bit[2]=(result>>2)&0x1;
+	bit[3]=(result>>3)&0x1;
+	bit[4]=(result>>4)&0x1;
+	bit[5]=(result>>5)&0x1;
+	bit[6]=(result>>6)&0x1;
+	bit[7]=(result>>7)&0x1;
+	cpu.PF = ~(bit[0]^bit[1]^bit[2]^bit[3]^bit[4]^bit[5]^bit[6]^bit[7]);
+    if(result<0)
+		cpu.SF = 1;
+	else
+		cpu.SF = 0;
+	if(result == 0)
+		cpu.ZF = 1;
+	
+	if((result > 0&&(op_dest->val)<0&&op_src->val>0)||(result < 0&&(op_dest->val)>0&&op_src->val<0))
+		{
+			cpu.OF = 1;
+		}
+	else{
+			cpu.OF = 0;
+		}
+	
+	 DATA_TYPE val1 = op_dest -> val & 0xf;
+     DATA_TYPE val2 = op_src -> val & 0xf;
+		if((unsigned)val1 < (unsigned)val2)
+			{
+			cpu.AF = 1;
+		    }
+		else{
+			cpu.AF = 0;
+		    }
+		
+		if( (unsigned)(op_dest->val) < (unsigned)(op_src->val) )
+			{
+				cpu.CF = 1;
+			}
+		else{
+				cpu.CF = 0;
+			}
 
 	print_asm_template2();
 }
