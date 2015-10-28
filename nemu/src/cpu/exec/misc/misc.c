4802d4f14aca052511d9e821362e261c4fb97b6d
tracer-ics2015 <tracer@njuics.org>
2015-10-28 16:05:14 +0800

    >  compile NEMU 141220000 yixiaodong Linux yixiaodong-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  16:05:13 up  1:02,  2 users,  load average: 1.28, 0.67, 0.56 9e19a2846877e8c67bd636a347603c932cc57c3e

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -23,3 +23,10 @@ make_helper(lea) {
 	print_asm("leal %s,%%%s", op_src->str, regsl[m.reg]);
 	return 1 + len;
 }
+make_helper(ret) {
+	cpu.eip = swaddr_read(cpu.esp,4);
+	cpu.esp += 4;
+	return 5;
+}
+
+
